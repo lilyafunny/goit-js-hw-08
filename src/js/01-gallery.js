7bb40911bbf9f329c7bcbb1eb883f5c8e1533fef
lilyafunny <125149545+lilyafunny@users.noreply.github.com>
2023-09-30 23:37:56 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const ulEl = document.querySelector('.gallery');
+function galleryImg(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+ulEl.insertAdjacentHTML('beforeend', galleryImg(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
